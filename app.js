c157818f41ac35c89795cc4008afb3bf2a8baa78
Ailea Patrinely <aileapatrinely@gmail.com>
2020-06-29 14:01:32 -0500

    delete btn removes div

--- app.js
+++ app.js
@@ -5,16 +5,21 @@ let clickCount = 0;
 function onReady() {
   $('.js-generate-btn').on('click', appendDiv);
   $('#allthedivs').on('click', '.yellow-btn', toggleColor);
+  $('#allthedivs').on('click', '.dadiv', deletediv);
 }
 
 function appendDiv() {
   clickCount++;
 
-  $('#allthedivs').append(`<div>
+  $('#allthedivs').append(`<div class='dadiv'>
   <p>${clickCount}</p><button class="yellow-btn">Yellow</button><button class="delete-da-btn">Delete</button>
   </div>`);
 }
 
 function toggleColor() {
   $(this).parent().toggleClass('yellow');
+}
+
+function deletediv() {
+  $(this).remove();
 }
